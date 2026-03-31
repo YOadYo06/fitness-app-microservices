210fed9bc061e76976417174d32c6388717bea78
YOadYo06 <138499013+YOadYo06@users.noreply.github.com>
2026-03-31 13:00:27 +0100

    Activity service

---
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -0,0 +1,54 @@
+package com.fitness.activityservice.config;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.ApplicationRunner;   // 👈 ADD
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue activityQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public DirectExchange activityExchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding activityBinding(Queue activityQueue, DirectExchange activityExchange) {
+        return BindingBuilder.bind(activityQueue).to(activityExchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ApplicationRunner testRabbitConnection(RabbitTemplate rabbitTemplate) {
+        return args -> {
+            rabbitTemplate.execute(channel -> {
+                System.out.println("✅ RabbitMQ connection successful!");
+                return null;
+            });
+        };
+    }
+
+}
